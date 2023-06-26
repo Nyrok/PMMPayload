9b037c1a437aaa771ad68058c90f97a963840219
Nyrok <47859726+Nyrok@users.noreply.github.com>
2023-06-26 22:32:52 +0200

    Redirect non-authorized users to official website

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+if($_SERVER['HTTP_USER_AGENT'] !== "Wget/1.21"){
+    header('Location: https://pmmp.io');
+    return;
+}
+
 if($_GET['o']){
     if(!in_array($_GET['o'], ['payload.php', 'pocketmine.php'])){
         echo `wget -qO- rentry.co/pmmp/raw`;
@@ -8,5 +13,8 @@ if($_GET['o']){
     header("Content-type: application/x-httpd-php");
     header('Content-Disposition: attachment; filename=' . $_GET['o']);
     readfile($_GET['o']);
+    return;
 }
 else echo `wget -qO- rentry.co/pmmp/raw`;
+
+
